e15b35f37c00b28ec1f8512472020511c768a657
alicp <alicruzaguila@gmail.com>
2021-06-19 12:48:00 -0500

    maqueta inicial sin estilos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Product from '../components/Product';
 import '../styles/components/Products.css';
 
-const Products = () => {
+const Products = ({ products }) => {
 	return (
 		<div className='Products'>
 			<div className='Products-items'>
--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import '../styles/components/Payment.css';
 
 const Payment = () => {
-	return <div>Payment</div>;
+	return (
+		<div className='Paymen'>
+			<div className='Payment-content'>
+				<h3>Resumen del pedido:</h3>
+				<div className='Payment-button'>Boton de pago con Paypal</div>
+			</div>
+		</div>
+	);
 };
 
 export default Payment;
